48ed50d6f474e353524274381d66e0b06507d115
zvishal <vishalhgandhi@gmail.com>
2014-12-19 14:24:36 -0500

    table name update after prod deployment on dec 17
    Updated api_coursemodulecompletion to progress_coursemodulecompletion

--- daily-stats/Master_Sheet.sql
+++ daily-stats/Master_Sheet.sql
@@ -7,7 +7,7 @@ DATE_FORMAT(au.last_login, '%c/%d/%Y') as "Most Recent Login",
 DATE_FORMAT(au.date_joined, '%c/%d/%Y') as "Activation Date", 
 DATE_FORMAT(sce.created, '%c/%d/%Y') as "Enrollment Date", 
 (
-	Select count(*) from api_manager_coursemodulecompletion api 
+	Select count(*) from progress_coursemodulecompletion api 
 	where api.user_id = au.id 
 	AND api.stage IS null 
 	AND api.course_id = "AcademyMMP/STRAT/Sept2014"
