b00fe219621e2b16a2004a7b31954a8da0e7a206
zvishal <vishalhgandhi@gmail.com>
2015-01-30 15:34:01 -0500

    added ta review and peer review

--- course-grades/grades.sql
+++ course-grades/grades.sql
@@ -28,3 +28,29 @@ AND AU.id NOT IN
 (Select sc.user_id from student_courseaccessrole as sc 
 WHERE sc.course_id = "AcademyMMP/STRAT/Sept2014") 
 ORDER BY AU.id
+
+/*TA REVIEW */
+SELECT pwr.id,pwr.workgroup_id, pwr.content_id, SM.id, SM.module_id,
+AU.username,SM.grade AS "final_score", 
+pwr.question,pwr.answer as "mean_scores"   
+FROM auth_user AU
+LEFT JOIN courseware_studentmodule as SM ON SM.student_id = AU.id
+LEFT JOIN projects_workgroup_users as pwu ON pwu.user_id = SM.student_id
+LEFT JOIN projects_workgroupreview as pwr ON pwu.workgroup_id = pwr.workgroup_id
+WHERE SM.module_type = 'group-project' 
+AND SM.course_id='AcademyMMP/MAPS/Nov2014'
+AND pwr.content_id LIKE '%AcademyMMP/MAPS%'
+ORDER BY SM.grade, AU.username, pwr.question
+
+/*PEER REVIEW*/
+SELECT pwr.id,pwr.workgroup_id, pwr.content_id, SM.id, SM.module_id,
+AU.username,SM.grade AS "final_score", 
+pwr.question,pwr.answer as "mean_scores"   
+FROM auth_user AU
+LEFT JOIN courseware_studentmodule as SM ON SM.student_id = AU.id
+LEFT JOIN projects_workgroup_users as pwu ON pwu.user_id = SM.student_id
+LEFT JOIN projects_workgrouppeerreview as pwr ON pwu.workgroup_id = pwr.workgroup_id
+WHERE SM.module_type = 'group-project' 
+AND SM.course_id='AcademyMMP/MAPS/Nov2014'
+AND pwr.content_id LIKE '%AcademyMMP/MAPS%'
+ORDER BY SM.grade, AU.username, pwr.question
